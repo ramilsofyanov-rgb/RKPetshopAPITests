e29f2651a1d153c19c9c9819902ab51b75ee0b57
ramilsofyanov-rgb <ramilsofyanov@gmail.com>
2026-06-01 22:06:53 +0700

    Сделал правки в тестах по комментариям от куратора. Создал файлы inventory_schema и order_schema

--- tests/test_store.py
+++ tests/test_store.py
@@ -1,5 +1,10 @@
 import allure
+import jsonschema
 import requests
+import pytest
+
+from tests.schemas.inventory_schema import INVENTORY_SCHEMA
+from tests.schemas.order_schema import ORDER_SCHEMA
 
 BASE_URL = "http://5.181.109.28:9090/api/v3"
 
@@ -17,39 +22,43 @@ class TestStore:
             }
         with allure.step("Отправка запроса на размещение заказа"):
             response = requests.post(url= f"{BASE_URL}/store/order", json= payload)
+            response_json = response.json()
 
-        with allure.step("Проверка статуса ответа"):
+        with allure.step("Проверка статуса ответа и валидация JSON-схемы"):
             assert response.status_code == 200
+            jsonschema.validate(response_json, ORDER_SCHEMA)
 
-        with allure.step("Проверка, что ответ содержит данные заказа"):
-            response.data = response.json()
-            assert response.data["id"] == payload["id"], "id заказа не совпадает с ожидаемым"
-            assert response.data["petId"] == payload["petId"],"petId заказа не совпадает с ожидаемым"
-            assert response.data["quantity"] == payload["quantity"], "quantity заказа не совпадает с ожидаемым"
-            assert response.data["status"] == payload["status"], "status заказа не совпадает с ожидаемым"
-            assert response.data["complete"] == payload["complete"], "complete заказа не совпадает с ожидаемым"
+        with allure.step("Проверка параметров заказа в ответе"):
+            assert response_json["id"] == payload["id"], "id заказа не совпадает с ожидаемым"
+            assert response_json["petId"] == payload["petId"],"petId заказа не совпадает с ожидаемым"
+            assert response_json["quantity"] == payload["quantity"], "quantity заказа не совпадает с ожидаемым"
+            assert response_json["status"] == payload["status"], "status заказа не совпадает с ожидаемым"
+            assert response_json["complete"] == payload["complete"], "complete заказа не совпадает с ожидаемым"
 
     @allure.title("Получение информации о заказе по ID")
-    def test_get_order_by_id(self):
+    def test_get_order_by_id(self, create_order):
+        with allure.step("Получение ID созданного заказа"):
+            order_id = create_order["id"]
         with allure.step("Отправка запроса на получение информации о заказе по ID"):
-            response = requests.get(url= f"{BASE_URL}/store/order/1")
+            response = requests.get(url= f"{BASE_URL}/store/order/{order_id}")
 
-        with allure.step("Проверка статуса ответа"):
+        with allure.step("Проверка статуса ответа и данных заказа"):
             assert response.status_code == 200
-
-        with allure.step("Проверка, что ответ содержит данные заказа с id = 1"):
-            assert response.json()["id"] == 1
+            assert response.json()["id"] == order_id
 
     @allure.title("Удаление заказа по ID")
-    def test_delete_order_by_id(self):
+    def test_delete_order_by_id(self, create_order):
+        with allure.step("Получение ID созданного заказа"):
+            order_id = create_order["id"]
+
         with allure.step("Отправка запроса на удаление заказа по ID"):
-            response = requests.delete(url= f"{BASE_URL}/store/order/1")
+            response = requests.delete(url= f"{BASE_URL}/store/order/{order_id}")
 
         with allure.step("Проверка статуса ответа"):
             assert response.status_code == 200
 
         with allure.step("Проверка запроса на получение информации о удаленном заказе"):
-            response = requests.get(url= f"{BASE_URL}/store/order/1")
+            response = requests.get(url= f"{BASE_URL}/store/order/{order_id}")
 
         with allure.step("Проверка статуса ответа"):
             assert response.status_code == 404
@@ -72,6 +81,5 @@ class TestStore:
 
         with allure.step("Проверка, что ответ содержит данные инвентаря"):
             data = response.json()
-            assert type(data["approved"]) == int
-            assert type(data["delivered"]) == int
+            jsonschema.validate(data, INVENTORY_SCHEMA)
 
